71d99b51eb8f441c6a6a2736794ce18857daec45
fx900-fx97@7 <ss365493@gmail.com>
2023-01-29 17:12:53 +0530

    create server http module and fs module in node with fs.writeFileSync and fs.unlikeSync functions with process.argv[indexofcmd]

---
+++ 4node_http_server/data.js
@@ -0,0 +1,11 @@
+const data = [
+    {name:'sagar',email:'some@emil.com'},
+    {name:'mrinal',email:'mrinal@emil.com'},
+    {name:'vikash',email:'vikash@emil.com'},
+    {name:'suman',email:'suman@emil.com'},
+    {name:'yuvraj',email:'yuvi@emil.com'},
+    {name:'sunay',email:'sunay@emil.com'},
+]
+
+
+module.exports = data
