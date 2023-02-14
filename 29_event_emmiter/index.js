7daa1109f623cc54215f262e9919f760b4891701
fx900-fx97@7 <ss365493@gmail.com>
2023-02-14 21:28:32 +0530

    event emitter

---
+++ 29_event_emmiter/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import EventEmitter from 'events'
+
+const app = express()
+const event = new EventEmitter()
+let count = 0
+event.on("countAPI",()=>{
+    count++;
+    console.log(`event called ${count}`)
+})
+
+app.get('/',(req,res)=>{
+    res.send('home')
+    event.emit('countAPI')
+})
+
+app.get('/search',(req,res)=>{
+    res.send('search')
+})
+
+app.get('/update',(req,res)=>{
+    res.send('update')
+})
+
+app.listen(5000)
